d6203a6dca5ca559df6eda26d7655b8b8bdcb73f
Vinicius Gabriel <viniciusxime@gmail.com>
2023-04-12 12:06:29 -0300

    incomplete drag parallax slider

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 let images = [...document.querySelectorAll('.img')];
 let slider = document.querySelector('.slider-exposicao-empresa');
+let innerSlider = document.querySelector('.slider-inner');
+
 let sliderWidth;
 let imageHeight;
 let current = 0;
 let target = 0;
 let ease = .05;
-var header = document.getElementById("header-sticky");
-window.onscroll = header.classList.add("sticky");
+
+let pressed = false
+let startx;
+let x;
+
+slider.addEventListener('mousedown', (e) => {
+    pressed = true;
+    startx = e.offsetX - innerSlider.offsetLeft;
+    innerSlider.style.cursor = 'grabbing'
+})
+
+slider.addEventListener('mouseenter', () => {
+    innerSlider.style.cursor = 'grab'
+})
+
+slider.addEventListener('mouseup', () => {
+    innerSlider.style.cursor = 'grab'
+})
+
+slider.addEventListener('mouseup', () => {
+    pressed = false;
+})
+
+slider.addEventListener('mousemove', (e) => {
+    if(!pressed) return;
+    e.preventDefault();
+
+    x = e.offsetX
+
+    innerSlider.style.left = `${x - startx}px`;
+})
+
+window.addEventListener('resize', init);
 
 images.forEach((img, index) => {
     img.style.backgroundImage = `url(./images/parallax-${index+1}.jpg)`
@@ -26,13 +59,24 @@ function init() {
     document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
 }
 
-function animate() {
-    current = parseFloat(lerp(current, target, ease)).toFixed(2);
-    target = window.scrollY;
-    setTransform(slider, `translateX(-${current}px)`)
-    requestAnimationFrame(animate);
-}
 
-// stickyHeader();
-init();
-animate();
+// function animate() {
+//     current = parseFloat(lerp(current, target, ease)).toFixed(2);
+//     target = window.scrollY;
+//     setTransform(slider, `translateX(-${current}px)`)
+//     animateImages();
+//     requestAnimationFrame(animate);
+// }
+
+// function animateImages() {
+//     let ratio = current / imageWidth;
+//     let intersectionRatioValue;
+
+//     images.forEach((image, index) => {
+//         intersectionRatioValue = ratio - (index * 0.5);
+//         setTransform(image, `translateX(${intersectionRatioValue * 70}px)`)
+//     })
+// }
+
+// init();
+// animate();
