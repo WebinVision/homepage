470151fb3300ac354cc62327b641e44cd4be7f20
Vinicius Gabriel <viniciusxime@gmail.com>
2023-04-19 12:17:29 -0300

    styling parallax slider

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let images = [...document.querySelectorAll('.img')];
+let images2 = [...document.querySelectorAll('.img2')];
 let slider = document.querySelector('.slider-exposicao-empresa');
-let innerSlider = document.querySelector('.slider-inner');
+let slider2 = document.querySelector('.slider-exposicao-empresa');
 
 let sliderWidth;
+let sliderWidth2;
 let imageHeight;
 let current = 0;
 let target = 0;
@@ -18,6 +20,10 @@ images.forEach((img, index) => {
     img.style.backgroundImage = `url(./images/parallax-${index+1}.jpg)`
 })
 
+images2.forEach((img2, index) => {
+    img2.style.backgroundImage = `url(./images/second-parallax-${index[2]+1}.jpg)`
+})
+
 function lerp(start, end, t) {
     return start * (1-t) + end * t;
 }
@@ -28,6 +34,7 @@ function setTransform(element, transform) {
 
 function init() {
     sliderWidth = slider.getBoundingClientRect().width;
+    // sliderWidth2 = slider2.
     imageWidth = sliderWidth / images.length;
     // document.body.style.height = `${sliderWidth - (3*(window.innerWidth) - window.innerHeight)}px`
 }
@@ -45,7 +52,7 @@ function animateImages() {
     let intersectionRatioValue;
 
     images.forEach((image, index) => {
-        intersectionRatioValue = ratio - (index * 0.5);
+        intersectionRatioValue = ratio - (index * 0.8);
         setTransform(image, `translateX(${intersectionRatioValue * 80}px)`)
     })
 }
