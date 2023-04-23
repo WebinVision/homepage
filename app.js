c210a691a8bc9649ba98568ffde2849d08a3156c
viniciusgjusta <viniciusxime@gmail.com>
2023-04-22 23:48:57 -0300

    completed homepage structure

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let images = [...document.querySelectorAll('.img')];
+let images2 = [...document.querySelectorAll('.img2')];
 let slider = document.querySelector('.slider-exposicao-empresa');
-let innerSlider = document.querySelector('.slider-inner');
+let slider2 = document.querySelector('.slider-exposicao-empresa-2');
 
 let sliderWidth;
+let sliderWidth2;
 let imageHeight;
 let current = 0;
 let target = 0;
@@ -18,6 +20,10 @@ images.forEach((img, index) => {
     img.style.backgroundImage = `url(./images/parallax-${index+1}.jpg)`
 })
 
+images2.forEach((img2, index) => {
+    img2.style.backgroundImage = `url(./images/second-parallax-${index+1}.jpg)`
+})
+
 function lerp(start, end, t) {
     return start * (1-t) + end * t;
 }
@@ -28,25 +34,35 @@ function setTransform(element, transform) {
 
 function init() {
     sliderWidth = slider.getBoundingClientRect().width;
+    sliderWidth2 = slider2.getBoundingClientRect().width;
     imageWidth = sliderWidth / images.length;
-    // document.body.style.height = `${sliderWidth - (3*(window.innerWidth) - window.innerHeight)}px`
+    imageWidth2 = sliderWidth2 / images2.length;
+    // document.body.style.height = `${sliderWidth - (3*(window.innerWidth) - window.innerHeight)}px`s
 }
 
 function animate() {
     current = parseFloat(lerp(current, target, ease)).toFixed(2);
     target = window.scrollY;
     setTransform(slider, `translateX(-${current}px)`)
+    setTransform(slider2, `translateX(${current}px)`)
     animateImages();
     requestAnimationFrame(animate);
 }
 
 function animateImages() {
     let ratio = current / imageWidth;
+    let ratio2 = current / imageWidth2;
     let intersectionRatioValue;
+    let intersectionRatioValue2;
 
     images.forEach((image, index) => {
-        intersectionRatioValue = ratio - (index * 0.5);
+        intersectionRatioValue = ratio - (index * 0.8);
         setTransform(image, `translateX(${intersectionRatioValue * 80}px)`)
+    })
+
+    images2.forEach((image, index) => {
+      intersectionRatioValue2 = ratio2 - (index * 0.8);
+      setTransform(image, `translateX(${intersectionRatioValue2 * 80}px)`)
     })
 }
 
